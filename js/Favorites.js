c745620eba975233f41a00ab28f73a1a6aa5f312
Fabio Flauzino <fabio@mfrural.com.br>
2024-06-20 10:46:01 -0300

    finalizado aplicação

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,29 +1,41 @@
-export class GithubUser {
-  static search(userName) {
-    const endpoint = `https://api.github.com/users/${userName}`
-
-    return fetch(endpoint)
-      .then(data => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers
-      }))
-  }
-}
+import { GithubUser } from "./GithubUser.js"
 
 //classe que vai conter a lógica dos dados
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
-
-    
   }
 
   load() {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  async add(username) {
+    const userExists = this.entries.find(entry => entry.login === username)
+
+    if (userExists) {
+      throw new Error("Usuário já cadastrado")
+    }
+
+    try {
+      const user = await GithubUser.search(username)
+
+      if (user.name === undefined) {
+        throw new Error("Usuário não encontrado")
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
   }
 
   delete(user) {
@@ -33,6 +45,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -42,6 +55,15 @@ export class FavoritesView extends Favorites {
     super(root)
     this.tbody = this.root.querySelector("table tbody")
     this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector(".search button")
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input")
+      this.add(value)
+    }
   }
 
   update() {
@@ -52,6 +74,7 @@ export class FavoritesView extends Favorites {
 
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
       row.querySelector('.user p').textContent = user.name
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
